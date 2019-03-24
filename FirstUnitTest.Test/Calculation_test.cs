a5ea253b75841220f324036aeec3a2b54e8c0f07
Parang Pradopo <pradopo@gmail.com>
2019-03-24 20:08:55 +0800

    Add project files.

---
+++ FirstUnitTest.Test/Calculation_test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using FirstUnitTest;
+
+namespace FirstUnitTest.Test
+{
+    public class Calculation_test
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            var cal = new FirstUnitTest.Calculation();
+            Assert.Equal(4, cal.Add(2, 2));
+
+        }
+
+        [Fact]
+        public void TestMultiple()
+        {
+            var cal = new FirstUnitTest.Calculation();
+            Assert.Equal(6, cal.Multiple(2, 3));
+
+        }
+
+        [Fact]
+        public void TestDevideByZero()
+        {
+            var cal = new FirstUnitTest.Calculation();
+            Assert.Throws<DivideByZeroException>(() => cal.Divide(2, 0));
+
+        }
+
+        [Fact]
+        public void TestDevide()
+        {
+            var cal = new FirstUnitTest.Calculation();
+            Assert.Equal(10,cal.Divide(20, 2));
+
+        }
+    }
+}
---
+++ FirstUnitTest/Calculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUnitTest
+{
+    public class Calculation
+    {
+        public int Add(int inputA, int inputB)
+        {
+            return inputA + inputB;
+        }
+
+        public int Multiple(int inputA, int inputB)
+        {
+            return inputA * inputB;
+        }
+
+        public int Divide(int inputA, int inputB)
+        {
+            if (inputB == 0)
+            {
+                throw new DivideByZeroException("input B should have value");
+            }
+            else
+            {
+                return inputA / (inputB);
+            }
+
+        }
+    }
+}
